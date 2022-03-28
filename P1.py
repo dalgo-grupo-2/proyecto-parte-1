f1b20c635d948a27645a0e2041c3177e2d57304c
juanyepesp <j.yepes@uniandes.edu.co>
2022-03-27 22:13:53 -0500

    primer acercamiento

---
+++ P1.py
@@ -0,0 +1,47 @@
+import sys
+
+nuestroGrafo = {
+    ("11","12"):2,
+    ("12","11"):2,
+    ("12","13"):2,
+    ("13","12"):2,
+    
+    ("21","22"):1,
+    ("22","21"):1,
+    ("22","23"):1,
+    ("23","22"):1,
+    
+    ("31","32"):3,
+    ("32","31"):3,
+    ("32","33"):3,
+    ("33","32"):3,
+
+    ("41","42"):0,
+    ("42","41"):0,
+    ("42","43"):0,
+    ("43","42"):0,
+
+    ("12","31"):0,
+    ("13","43"):0,
+    ("21","42"):0,
+    ("32","41"):0
+}
+
+def graph():
+    hecho=True
+    debilitamiento = True
+    Djikstra = True
+    espacioTrabajo = None
+    pensarlo = True
+    implementacion = False
+    proyecto = None
+    documentoAnalisis = None
+    deOne = 1000000000
+    loLograremos = 0.50
+    return
+
+
+def torreDeTeletransportacion():
+    
+    return
+
